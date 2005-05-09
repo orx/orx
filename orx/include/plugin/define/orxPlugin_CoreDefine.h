ba9ff73b7268129f116d38fea800f19c206e828a
iarwain <iarwain@orx-project.org>
2005-05-09 20:46:57 +0000

    - Merged with unified plugins system

---
+++ orx/include/plugin/define/orxPlugin_CoreDefine.h
@@ -0,0 +1,42 @@
+/**
+ * @file orxPlugin_CoreDefine.h
+ * 
+ * Header that regroups all core plugin defines.
+ */ 
+ 
+ /***************************************************************************
+ orxPlugin_CoreDefine.h
+ 
+ begin                : 09/05/2005
+ author               : (C) Arcallians
+ email                : iarwain@arcallians.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#ifndef _orxPLUGIN_COREDEFINE_H_
+#define _orxPLUGIN_COREDEFINE_H_
+
+/*
+ * Includes all core plugin headers
+ */
+#include "plugin/define/orxPlugin_Display.h"
+#include "plugin/define/orxPlugin_File.h"
+#include "plugin/define/orxPlugin_Joystick.h"
+#include "plugin/define/orxPlugin_Keyboard.h"
+#include "plugin/define/orxPlugin_Mouse.h"
+#include "plugin/define/orxPlugin_Package.h"
+#include "plugin/define/orxPlugin_Script.h"
+#include "plugin/define/orxPlugin_Sound.h"
+#include "plugin/define/orxPlugin_Timer.h"
+
+
+#endif /*_orxPLUGIN_COREDEFINE_H_*/
