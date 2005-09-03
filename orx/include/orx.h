97d3943f56c62a50d5b90122ff5e76b2a931f31e
bestel <none@none>
2005-09-03 23:20:23 +0000

    Added include file for User Plugins (and precompiled header)

---
+++ orx/include/orx.h
@@ -0,0 +1,84 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+/**
+ * @file
+ * @date 02/09/2005
+ * @author (C) Arcallians
+ * 
+ */
+
+#ifndef _orx_H_
+#define _orx_H_
+
+/* Include all the module of the project.
+ * This file will be used to generate a precompiled header for compiler that
+ * support this feature.
+ * This header is also the only file to include inside user plugin
+ */
+
+#include "anim/orxAnim.h"
+#include "anim/orxAnimPointer.h"
+#include "anim/orxAnimSet.h"
+
+#include "base/orxDecl.h"
+#include "base/orxType.h"
+
+#include "camera/orxCamera.h"
+#include "camera/orxRender.h"
+#include "camera/orxViewport.h"
+
+#include "core/orxClock.h"
+#include "core/orxEvent.h"
+#include "core/orxTime.h"
+
+#include "debug/orxDebug.h"
+#include "debug/orxFps.h"
+
+#include "display/graphic.h"
+#include "display/orxDisplay.h"
+#include "display/orxTexture.h"
+
+#include "io/interaction.h"
+#include "io/orxFile.h"
+#include "io/orxJoystick.h"
+#include "io/orxKeyboard.h"
+#include "io/orxMouse.h"
+#include "io/orxPackage.h"
+#include "io/orxTextIO.h"
+
+#include "main/orxMain.h"
+
+#include "object/orxFrame.h"
+#include "object/orxObject.h"
+#include "object/orxStructure.h"
+
+#include "plugin/orxPlugin.h"
+#include "plugin/orxPluginCore.h"
+#include "plugin/orxPluginUser.h"
+
+#include "script/orxScript.h"
+
+#include "sound/orxSound.h"
+
+#include "utils/orxBayes.h"
+#include "utils/orxFSM.h"
+#include "utils/orxHashTable.h"
+#include "utils/orxLinkList.h"
+#include "utils/orxQueue.h"
+#include "utils/orxScreenshot.h"
+#include "utils/orxString.h"
+#include "utils/orxTest.h"
+#include "utils/orxTree.h"
+#include "utils/pathfinder.h"
+
+
+#endif /*_orx_H_*/
+
+/** @} */
