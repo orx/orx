17232f01d02d301f4819a7232ecca3e89dad7502
corrosifpirate <none@none>
2005-09-04 13:32:18 +0000

    orxDebug.h included to avoid compile error.

--- orx/src/main/orxMain.c
+++ orx/src/main/orxMain.c
@@ -25,6 +25,7 @@
 
 #include "main/orxMain.h"
 #include "memory/orxMemory.h"
+#include "debug/orxDebug.h"
 
 #define orxMAIN_KU32_FLAG_NONE  0x00000000  /**< No flags have been set */
 #define orxMAIN_KU32_FLAG_READY 0x00000001  /**< The module has been initialized */
