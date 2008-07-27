22da9d9e786015849cee0c51c6726fa6a88feab5
iarwain <iarwain@orx-project.org>
2008-07-27 00:48:56 +0000

    - Updated SDL plugin files with new headers

--- orx/plugins/Display/SDL/orxDisplay.c
+++ orx/plugins/Display/SDL/orxDisplay.c
@@ -1,23 +1,34 @@
-/**
- * @file orxDisplay.c
+/* Orx - Portable Game Engine
  *
- * SDL display plugin
+ * Orx is the legal property of its developers, whose names
+ * are listed in the COPYRIGHT file distributed 
+ * with this source distribution.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-/***************************************************************************
- begin                : 14/11/2003
- author               : (C) Arcallians
- email                : iarwain@arcallians.org
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This library is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Lesser General Public License           *
- *   as published by the Free Software Foundation; either version 2.1      *
- *   of the License, or (at your option) any later version.                *
- *                                                                         *
- ***************************************************************************/
+/**
+ * @file orxDisplay.c
+ * @date 14/11/2003
+ * @author iarwain@orx-project.org
+ *
+ * @todo
+ *
+ * SDML display plugin implementation
+ *
+ */
 
 
 #include "orxInclude.h"
