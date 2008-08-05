608b747b746ae8be36b1b5a301fcc2ec8fa10c3f
iarwain <iarwain@orx-project.org>
2008-08-05 17:20:44 +0000

    - Added more info / options to the first tutorial

--- tutorial/src/01_Object/01_Object.c
+++ tutorial/src/01_Object/01_Object.c
@@ -31,11 +31,40 @@
 #include "orx.h"
 
 
+/* This is the basic C tutorial creating a viewport and an object.
+ * As we are using the default executable for this tutorial, this code
+ * will be loaded and executed as a runtime plugin.
+ *
+ * In addition, some basics are handled for us by the main executable.
+ * First of all, it will load all available plugins and modules. If you
+ * require only some of those, then it's better to write your own executable
+ * instead of a plugin. This will be covered in a later tutorial.
+ *
+ * The main executable also handles some keys:
+ * - F11 as vertical sync toggler
+ * - Escape as exit key
+ * - Backspace to reload all configuration files (provided that config history is turned on)
+ * It also exits if the orxSYSTEM_EVENT_CLOSE signal is sent.
+ *
+ * As orx is data driven, here we just write 2 lines of code to create a viewport
+ * and an object, but all their properties can be defined in the config file (01_Object.ini).
+ * As a matter of fact, the viewport is associated with a camera implicitly created from the
+ * info given in the config file. You can also set their sizes, positions, the object colors,
+ * scales, rotations, animations, physical properties, and so on. You can even request
+ * random values for these without having to add a single line of code.
+ * In a later tutorial we'll see how to generate your whole scene (all background
+ * and landscape objects for example) with a simple for loop written in 3 lines of code.
+ *
+ * For now, you can try to uncomment some of the lines of 01_Object.ini, play with them,
+ * then relaunch this tutorial. For an exhaustive list of options, please look at CreationTemplate.ini.
+ */
+
+
 /** Inits the tutorial
  */
-orxSTATIC orxSTATUS Init()
+orxSTATUS Init()
 {
-  /* Loads config file and selects its section */
+  /* Loads config file */
   orxConfig_Load("../../01_Object/01_Object.ini");
 
   /* Creates viewport */
