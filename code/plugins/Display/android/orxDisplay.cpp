ac3a6ecc1f95e66d3ff9ec801d264512f99748d3
laschweinski <none@none>
2011-05-14 09:20:31 +0000

    remove some orxLOG

--- code/plugins/Display/android/orxDisplay.cpp
+++ code/plugins/Display/android/orxDisplay.cpp
@@ -7,7 +7,7 @@
 
 /* Orx - Portable Game Engine
  *
- * Copyright (c) 2008-2010 Orx-Project
+ * Copyright (c) 2008-2011 Orx-Project
  *
  * This software is provided 'as-is', without any express or implied
  * warranty. In no event will the authors be held liable for any damages
@@ -1924,8 +1924,6 @@ orxBITMAP *orxFASTCALL orxDisplay_android_LoadBitmap(const orxSTRING _zFilename)
 		/* Deletes surface */
 		SOIL_free_image_data(pu8ImageData);
 	}
-
-	orxDEBUG_PRINT(orxDEBUG_LEVEL_DISPLAY,"loading %d", pstResult);
 
 	/* Done! */
 	return pstResult;
