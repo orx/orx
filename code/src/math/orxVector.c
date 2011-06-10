be77a50f8c6035639d1a4445492e9a33e8f923c4
iarwain <iarwain@orx-project.org>
2011-06-10 09:22:25 +0000

    - Added orxVector_CatmullRom() function that computes points on a Catmull-Rom (spline) curve

--- code/src/math/orxVector.c
+++ code/src/math/orxVector.c
@@ -33,6 +33,48 @@
 #include "math/orxVector.h"
 
 
+/* *** Vector functions *** */
+
+/** Computes an interpolated point on a Catmull-Rom curve segment for a given parameter
+ * @param[out]  _pvRes                      Vector where to store result
+ * @param[in]   _pvPoint1                   First control point for this curve segment
+ * @param[in]   _pvPoint2                   Second control point for this curve segment
+ * @param[in]   _pvPoint3                   Third control point for this curve segment
+ * @param[in]   _pvPoint4                   Fourth control point for this curve segment
+ * @param[in]   _fT                         Interpolation parameter in [0.0, 1.0]
+ * @return      Interpolated point on the Catmull-Rom cureve segment
+ */
+orxVECTOR *orxFASTCALL orxVector_CatmullRom(orxVECTOR *_pvRes, const orxVECTOR *_pvPoint1, const orxVECTOR *_pvPoint2, const orxVECTOR *_pvPoint3, const orxVECTOR *_pvPoint4, orxFLOAT _fT)
+{
+  orxFLOAT fT2, fT3, fW1, fW2, fW3, fW4;
+
+  /* Checks */
+  orxASSERT(_pvRes != orxNULL);
+  orxASSERT(_pvPoint1 != orxNULL);
+  orxASSERT(_pvPoint2 != orxNULL);
+  orxASSERT(_pvPoint3 != orxNULL);
+  orxASSERT(_pvPoint4 != orxNULL);
+  orxASSERT((_fT >= orxFLOAT_0) && (_fT <= orxFLOAT_1));
+
+  /* Computes weights */
+  fT2 = _fT * _fT;
+  fT3 = fT2 * _fT;
+  fW1 = orx2F(2.0f) * fT2 - fT3 - _fT;
+  fW2 = orx2F(3.0f) * fT3 - orx2F(5.0f) * fT2 + orx2F(2.0f);
+  fW3 = orx2F(4.0f) * fT2 - orx2F(3.0f) * fT3 + _fT;
+  fW4 = fT3 - fT2;
+
+  /* Gets intermediate results */
+  orxVector_Set(_pvRes,
+                orx2F(0.5f) * (fW1 * _pvPoint1->fX + fW2 * _pvPoint2->fX + fW3 * _pvPoint3->fX + fW4 * _pvPoint4->fX),
+                orx2F(0.5f) * (fW1 * _pvPoint1->fY + fW2 * _pvPoint2->fY + fW3 * _pvPoint3->fY + fW4 * _pvPoint4->fY),
+                orx2F(0.5f) * (fW1 * _pvPoint1->fZ + fW2 * _pvPoint2->fZ + fW3 * _pvPoint3->fZ + fW4 * _pvPoint4->fZ));
+
+  /* Done! */
+  return _pvRes;
+}
+
+
 /* *** Vector constants *** */
 
 const orxVECTOR orxVECTOR_X      = {{orx2F(1.0f)}, {orx2F(0.0f)}, {orx2F(0.0f)}};
