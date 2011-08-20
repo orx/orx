d5f37715d746b265cb92380ef550bb64095918cb
loki666 <simons.philippe@gmail.com>
2011-08-20 17:45:36 +0000

    android: update demo

---
+++ code/demo/android/src/org/orxproject/orxtest/OrxDemo.java
@@ -0,0 +1,21 @@
+package org.orxproject.orxtest;
+
+import android.os.Bundle;
+
+import com.nvidia.devtech.NvEventQueueActivity;
+
+public class OrxDemo extends NvEventQueueActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	wantsAccelerometer = true;
+    	
+        super.onCreate(savedInstanceState);
+    }
+
+    static
+    {
+        System.loadLibrary("orxTest");
+    }
+
+}
