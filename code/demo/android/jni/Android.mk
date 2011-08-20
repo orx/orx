56aead605353efca5c7f4c2210bbe3e1f5db5474
loki666 <simons.philippe@gmail.com>
2011-08-20 17:48:17 +0000

    android: demo oops

--- code/demo/android/jni/Android.mk
+++ code/demo/android/jni/Android.mk
@@ -2,11 +2,10 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := orxTest  ## needs to be the same name in the meta-data tag android.orx.lib_name in the AndroidManifest.xml
-### Add all source file names to be included in lib separated by a whitespace
+LOCAL_MODULE    := orxTest
+
 LOCAL_SRC_FILES := orxTest.c
-LOCAL_STATIC_LIBRARIES := orxd-standalone
-LOCAL_SHARED_LIBRARIES := apkfile
+LOCAL_STATIC_LIBRARIES := orxd
 
 LOCAL_ARM_MODE := arm
 
--- code/demo/android/jni/Application.mk
+++ code/demo/android/jni/Application.mk
@@ -1,4 +1,3 @@
 APP_ABI := armeabi armeabi-v7a
 
-APP_MODULES := orxTest apkfile
-
+APP_PLATFORM := android-9
