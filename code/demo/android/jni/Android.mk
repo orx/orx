38adc6ddb31f8667e6d02ec273aa2f8e9931765a
loki666 <simons.philippe@gmail.com>
2011-08-21 19:18:58 +0000

    android: rebuild with platform android-5

--- code/demo/android/jni/Android.mk
+++ code/demo/android/jni/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := orxTest
 
 LOCAL_SRC_FILES := orxTest.c
-LOCAL_STATIC_LIBRARIES := orxd
+LOCAL_STATIC_LIBRARIES := orx
 
 LOCAL_ARM_MODE := arm
 
--- code/demo/android/jni/Application.mk
+++ code/demo/android/jni/Application.mk
@@ -1,3 +1,1 @@
 APP_ABI := armeabi armeabi-v7a
-
-APP_PLATFORM := android-9
