56d60b9501ea3fae3bfc4c5f07c361e9d12c663a
loki666 <simons.philippe@gmail.com>
2011-08-24 10:17:16 +0000

    android: Android.mk cleanup

--- code/build/android/Android.mk
+++ code/build/android/Android.mk
@@ -5,11 +5,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := orx
 LOCAL_SRC_FILES :=   $(TARGET_ARCH_ABI)/liborx.a
 LOCAL_EXPORT_LDLIBS := -L$(LOCAL_PATH)/../../../extern/SOIL/build/android/$(TARGET_ARCH_ABI) \
-                       -L$(LOCAL_PATH)/../../../extern/Box2D_2.1.3/build/android/$(TARGET_ARCH_ABI) \
                        -L$(LOCAL_PATH)/../../../extern/NvEvent/obj/local/$(TARGET_ARCH_ABI)
 LOCAL_EXPORT_LDLIBS += -llog -lGLESv2 -lnvmath -lnvglesutil \
                        -lnvhhdds -lnvlog -lnvshader -lnvfile -lnvapkfile -lnvevent -lnvtime -lnvthread \
-                       -lSOIL -lBox2D
+                       -lSOIL
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include $(LOCAL_PATH)/../../../extern/NvEvent/jni
 LOCAL_EXPORT_CFLAGS := -D__orxANDROID__
 LOCAL_EXPORT_CPPFLAGS := -D__orxANDROID__
@@ -21,11 +20,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := orxd
 LOCAL_SRC_FILES :=   $(TARGET_ARCH_ABI)/liborxd.a
 LOCAL_EXPORT_LDLIBS := -L$(LOCAL_PATH)/../../../extern/SOIL/build/android/$(TARGET_ARCH_ABI) \
-                       -L$(LOCAL_PATH)/../../../extern/Box2D_2.1.3/build/android/$(TARGET_ARCH_ABI) \
                        -L$(LOCAL_PATH)/../../../extern/NvEvent/obj/local/$(TARGET_ARCH_ABI)
 LOCAL_EXPORT_LDLIBS += -llog -lGLESv2 -lnvmath -lnvglesutil \
                        -lnvhhdds -lnvlog -lnvshader -lnvfile -lnvapkfile -lnvevent -lnvtime -lnvthread \
-                       -lSOIL -lBox2D
+                       -lSOIL
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include $(LOCAL_PATH)/../../../extern/NvEvent/jni
 LOCAL_EXPORT_CFLAGS := -D__orxANDROID__ -D__orxDEBUG__
 LOCAL_EXPORT_CPPFLAGS := -D__orxANDROID__ -D__orxDEBUG__
