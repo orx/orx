0899eb7d10e1c411528f7b76ee2f15f5206db9e2
loki666 <simons.philippe@gmail.com>
2011-08-30 20:50:00 +0000

    andoird: prebuild NvEvent lib

--- code/build/android/Android.mk
+++ code/build/android/Android.mk
@@ -5,9 +5,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := orx
 LOCAL_SRC_FILES :=   $(TARGET_ARCH_ABI)/liborx.a
 LOCAL_EXPORT_LDLIBS := -L$(LOCAL_PATH)/../../../extern/NvEvent/obj/local/$(TARGET_ARCH_ABI)
-LOCAL_EXPORT_LDLIBS += -llog -lGLESv2 -lnvmath -lnvglesutil \
-                       -lnvhhdds -lnvlog -lnvshader -lnvfile -lnvapkfile -lnvevent -lnvtime -lnvthread
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include $(LOCAL_PATH)/../../../extern/NvEvent/jni
+LOCAL_EXPORT_LDLIBS += -llog -lGLESv2
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include
 LOCAL_EXPORT_CFLAGS := -D__orxANDROID__
 LOCAL_EXPORT_CPPFLAGS := -D__orxANDROID__
 
@@ -18,9 +17,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := orxd
 LOCAL_SRC_FILES :=   $(TARGET_ARCH_ABI)/liborxd.a
 LOCAL_EXPORT_LDLIBS := -L$(LOCAL_PATH)/../../../extern/NvEvent/obj/local/$(TARGET_ARCH_ABI)
-LOCAL_EXPORT_LDLIBS += -llog -lGLESv2 -lnvmath -lnvglesutil \
-                       -lnvhhdds -lnvlog -lnvshader -lnvfile -lnvapkfile -lnvevent -lnvtime -lnvthread
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include $(LOCAL_PATH)/../../../extern/NvEvent/jni
+LOCAL_EXPORT_LDLIBS += -llog -lGLESv2
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include
 LOCAL_EXPORT_CFLAGS := -D__orxANDROID__ -D__orxDEBUG__
 LOCAL_EXPORT_CPPFLAGS := -D__orxANDROID__ -D__orxDEBUG__
 
--- code/demo/android/jni/Android.mk
+++ code/demo/android/jni/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := orxTest
 
 LOCAL_SRC_FILES := orxTest.c
-LOCAL_STATIC_LIBRARIES := orxd
+LOCAL_STATIC_LIBRARIES := orxd NvEvent
 LOCAL_SHARED_LIBRARIES := Box2D SOIL
 
 LOCAL_ARM_MODE := arm
@@ -15,3 +15,4 @@ include $(BUILD_SHARED_LIBRARY)
 $(call import-module,orx/code/build/android)
 $(call import-module,orx/extern/Box2D_2.1.3/build/android)
 $(call import-module,orx/extern/SOIL/build/android)
+$(call import-module,orx/extern/NvEvent/build)
