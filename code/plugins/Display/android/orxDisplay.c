c66fbf0ccc49846defef891c98fb5fd842cab4e4
loki666 <simons.philippe@gmail.com>
2011-09-13 17:26:48 +0000

    android: fix prepare bitmap

--- code/plugins/Display/android/orxDisplay.c
+++ code/plugins/Display/android/orxDisplay.c
@@ -807,13 +807,13 @@ static orxINLINE void orxDisplay_Android_PrepareBitmap(const orxBITMAP *_pstBitm
     /* Draws remaining items */
     orxDisplay_Android_DrawArrays();
 
+    /* Binds source's texture */
+    glBindTexture(GL_TEXTURE_2D, _pstBitmap->uiTexture);
+    glASSERT();
+
     /* No active shader? */
     if(sstDisplay.s32ActiveShaderCounter == 0)
     {
-      /* Binds source's texture */
-      glBindTexture(GL_TEXTURE_2D, _pstBitmap->uiTexture);
-      glASSERT();
-
       /* Stores it */
       sstDisplay.pstLastBitmap = _pstBitmap;
     }
