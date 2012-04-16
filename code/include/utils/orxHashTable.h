c224d1a4168ba96d488949ed82c64bb91dc1e4e6
iarwain <iarwain@orx-project.org>
2012-04-16 09:22:42 +0000

    - Removed orxHashTable_DebugPrint() as its output was completely worthless

--- code/include/utils/orxHashTable.h
+++ code/include/utils/orxHashTable.h
@@ -146,19 +146,6 @@ extern orxDLLAPI orxHANDLE orxFASTCALL          orxHashTable_FindNext(orxHASHTAB
 extern orxDLLAPI orxSTATUS orxFASTCALL          orxHashTable_Optimize(orxHASHTABLE *_pstHashTable);
 
 
-/*******************************************************************************
- * DEBUG FUNCTION
- ******************************************************************************/
-
-#ifdef __orxDEBUG__
-
-/** Print the content of a Hash table
- * @param[in] _pstHashTable  Hash table to display
- */
-extern orxDLLAPI void orxFASTCALL               orxHashTable_DebugPrint(const orxHASHTABLE *_pstHashTable);
-
-#endif /* __orxDEBUG__ */
-
 #endif /* _orxHASHTABLE_H_ */
 
 /** @} */
