c21812c018a4ec1d179ef962495bab996610af13
iarwain <iarwain@orx-project.org>
2012-12-06 01:34:16 -0800

    - Updated package-dev.py to allow different destination for folder copies and mixed folders/files with the same destination - Added a new slave in buildbot config for vs2010, mingw and doxygen

--- code/build/python/package-dev.py
+++ code/build/python/package-dev.py
@@ -43,45 +43,35 @@ docfileinfolist = [
 ]
 
 devfileinfolist = docfileinfolist + [
-    {'src': 'bin/*Template.ini',        'dst': None},
-    {'src': 'bin/orx*.ini',             'dst': None}
+    {'src': 'bin/*Template.ini',        'dst': '.'},
 ]
 
 vsfileinfolist = devfileinfolist + [
-    {'src': 'bin/orx*.exe',             'dst': None},
-    {'src': 'bin/orx*.dll',             'dst': None},
-    {'src': 'lib/dynamic/orx*.lib',     'dst': 'lib'}
+    {'src': 'lib/dynamic/orx*.dll',     'dst': 'lib'},
+    {'src': 'lib/dynamic/orx*.lib',     'dst': 'lib'},
+    {'src': 'lib/dynamic/orx*.pdb',     'dst': 'lib'}
 ]
 
 mingwfileinfolist = devfileinfolist + [
-    {'src': 'bin/orx*.exe',             'dst': None},
-    {'src': 'bin/orx*.dll',             'dst': None},
+    {'src': 'lib/dynamic/orx*.dll',     'dst': 'lib'},
     {'src': 'lib/dynamic/liborx*.a',    'dst': 'lib'}
 ]
 
 macfileinfolist = devfileinfolist + [
-    {'src': 'bin/orx',                  'dst': None},
-    {'src': 'bin/orxp',                 'dst': None},
-    {'src': 'bin/orxd',                 'dst': None},
-    {'src': 'bin/liborx*.dylib',        'dst': None},
     {'src': 'lib/dynamic/liborx*.dylib','dst': 'lib'}
 ]
 
 linuxfileinfolist = devfileinfolist + [
-    {'src': 'bin/orx',                  'dst': None},
-    {'src': 'bin/orxp',                 'dst': None},
-    {'src': 'bin/orxd',                 'dst': None},
-    {'src': 'bin/liborx*.so',           'dst': None},
     {'src': 'lib/dynamic/liborx*.so',   'dst': 'lib'}
 ]
 
-# List of folders
-docfolderlist = [
-    '../doc/html'
+# List of folder info
+docfolderinfolist = [
+    {'src': '../doc/html',              'dst': None}
 ]
 
-devfolderlist = [
-    'include'
+devfolderinfolist = [
+    {'src': 'include',                  'dst': None}
 ]
 
 
@@ -90,67 +80,98 @@ devfolderlist = [
 platforminfolist = {
 
     'vs2008': {
-        'name':         'dev-vs2008',
-        'format':       'zip',
-        'fileinfolist': vsfileinfolist,
-        'folderlist':   devfolderlist
+        'name':             'dev-vs2008',
+        'format':           'zip',
+        'fileinfolist':     vsfileinfolist,
+        'folderinfolist':   devfolderinfolist
     },
 
     'vs2010': {
-        'name':         'dev-vs2010',
-        'format':       'zip',
-        'fileinfolist': vsfileinfolist,
-        'folderlist':   devfolderlist
+        'name':             'dev-vs2010',
+        'format':           'zip',
+        'fileinfolist':     vsfileinfolist,
+        'folderinfolist':   devfolderinfolist
     },
 
     'mingw': {
-        'name':         'dev-mingw',
-        'format':       'zip',
-        'fileinfolist': mingwfileinfolist,
-        'folderlist':   devfolderlist
+        'name':             'dev-mingw',
+        'format':           'zip',
+        'fileinfolist':     mingwfileinfolist,
+        'folderinfolist':   devfolderinfolist
     },
 
     'mac': {
-        'name':         'dev-mac',
-        'format':       'zip',
-        'fileinfolist': macfileinfolist,
-        'folderlist':   devfolderlist
+        'name':             'dev-mac',
+        'format':           'zip',
+        'fileinfolist':     macfileinfolist,
+        'folderinfolist':   devfolderinfolist
     },
 
     'linux32': {
-        'name':         'dev-linux32',
-        'format':       'bztar',
-        'fileinfolist': linuxfileinfolist,
-        'folderlist':   devfolderlist
+        'name':             'dev-linux32',
+        'format':           'bztar',
+        'fileinfolist':     linuxfileinfolist,
+        'folderinfolist':   devfolderinfolist
     },
 
     'linux64': {
-        'name':         'dev-linux64',
-        'format':       'bztar',
-        'fileinfolist': linuxfileinfolist,
-        'folderlist':   devfolderlist
+        'name':             'dev-linux64',
+        'format':           'bztar',
+        'fileinfolist':     linuxfileinfolist,
+        'folderinfolist':   devfolderinfolist
     },
 
     'doxygen': {
-        'name':         'doc',
-        'format':       'zip',
-        'fileinfolist': docfileinfolist,
-        'folderlist':   docfolderlist
+        'name':             'doc',
+        'format':           'zip',
+        'fileinfolist':     docfileinfolist,
+        'folderinfolist':   docfolderinfolist
     }
 }
 
 
-### Cleans
+### Prepares
 
 # Deletes packing folder if present
 if os.path.exists(workdir):
     shutil.rmtree(workdir)
 
-
-### Copies files
-
 # Gets base destination path
 basedst = os.path.join(workdir, os.path.join('orx-' + version, platforminfolist[platform]['name']))
+
+
+### Copies folders
+
+# Should copy folders?
+if platforminfolist[platform]['folderinfolist']:
+
+    # Creates destination path if needed
+    if not os.path.exists(basedst):
+        os.makedirs(basedst)
+
+    # For all folder info for this platform
+    for folderinfo in platforminfolist[platform]['folderinfolist']:
+
+        # Gets destination
+        if folderinfo['dst']:
+            destination = os.path.join(basedst, folderinfo['dst'])
+        else:
+            destination = os.path.join(basedst, folderinfo['src'])
+
+        # Does the destination folder exist?
+        if os.path.exists(destination):
+
+            # Deletes it
+            shutil.rmtree(destination)
+
+        # Logs
+        print('Copying folder: ' + folderinfo['src'])
+
+        # Copies folder
+        shutil.copytree(os.path.join(basesrc, folderinfo['src']), destination)
+
+
+### Copies files
 
 # Should copy files?
 if platforminfolist[platform]['fileinfolist']:
@@ -180,24 +201,6 @@ if platforminfolist[platform]['fileinfolist']:
 
             # Copies file
             shutil.copy2(file, destination)
-
-
-### Copies folders
-
-# For all folder info for this platform
-for folder in platforminfolist[platform]['folderlist']:
-
-    # Does the destination folder exist?
-    if os.path.exists(os.path.join(basedst, folder)):
-
-        # Deletes it
-        shutil.rmtree(os.path.join(basedst, folder))
-
-    # Logs
-    print('Copying folder: ' + folder)
-
-    # Copies folder
-    shutil.copytree(os.path.join(basesrc, folder), os.path.join(basedst, folder))
 
 
 ### Exports archive
