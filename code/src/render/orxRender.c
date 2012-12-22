15d3f737a939db7cd1edbc87155d39f164746e9f
iarwain <iarwain@orx-project.org>
2012-12-22 05:56:47 -0800

    - Added commands: Render.GetWorldPosition & Render.GetScreenPosition

--- code/src/render/orxRender.c
+++ code/src/render/orxRender.c
@@ -34,6 +34,55 @@
 
 #include "plugin/orxPluginCore.h"
 
+#include "core/orxCommand.h"
+
+
+/***************************************************************************
+ * Private functions                                                       *
+ ***************************************************************************/
+
+/** Command: GetWorldPosition
+ */
+void orxFASTCALL orxRender_CommandGetWorldPosition(orxU32 _u32ArgNumber, const orxCOMMAND_VAR *_astArgList, orxCOMMAND_VAR *_pstResult)
+{
+  /* Gets world position */
+  orxRender_GetWorldPosition(&(_astArgList[0].vValue), &(_pstResult->vValue));
+
+  /* Done! */
+  return;
+}
+
+/** Command: GetScreenPosition
+ */
+void orxFASTCALL orxRender_CommandGetScreenPosition(orxU32 _u32ArgNumber, const orxCOMMAND_VAR *_astArgList, orxCOMMAND_VAR *_pstResult)
+{
+  /* Gets screen position */
+  orxRender_GetScreenPosition(&(_astArgList[0].vValue), orxNULL, &(_pstResult->vValue));
+
+  /* Done! */
+  return;
+}
+
+/** Registers all the render commands
+ */
+static orxINLINE void orxRender_RegisterCommands()
+{
+  /* Command: GetWorldPosition */
+  orxCOMMAND_REGISTER_CORE_COMMAND(Render, GetWorldPosition, "WorldPos", orxCOMMAND_VAR_TYPE_VECTOR, 1, 0, {"ScreenPos", orxCOMMAND_VAR_TYPE_VECTOR});
+  /* Command: GetScreenPosition */
+  orxCOMMAND_REGISTER_CORE_COMMAND(Render, GetScreenPosition, "ScreenPos", orxCOMMAND_VAR_TYPE_VECTOR, 1, 0, {"WorldPos", orxCOMMAND_VAR_TYPE_VECTOR});
+}
+
+/** Unregisters all the render commands
+ */
+static orxINLINE void orxRender_UnregisterCommands()
+{
+  /* Command: GetWorldPosition */
+  orxCOMMAND_UNREGISTER_CORE_COMMAND(Render, GetWorldPosition);
+  /* Command: GetScreenPosition */
+  orxCOMMAND_UNREGISTER_CORE_COMMAND(Render, GetScreenPosition);
+}
+
 
 /***************************************************************************
  * Public functions                                                        *
@@ -50,6 +99,7 @@ void orxFASTCALL orxRender_Setup()
   orxModule_AddDependency(orxMODULE_ID_RENDER, orxMODULE_ID_CLOCK);
   orxModule_AddDependency(orxMODULE_ID_RENDER, orxMODULE_ID_FPS);
   orxModule_AddDependency(orxMODULE_ID_RENDER, orxMODULE_ID_PROFILER);
+  orxModule_AddDependency(orxMODULE_ID_RENDER, orxMODULE_ID_COMMAND);
   orxModule_AddDependency(orxMODULE_ID_RENDER, orxMODULE_ID_STRUCTURE);
   orxModule_AddDependency(orxMODULE_ID_RENDER, orxMODULE_ID_VIEWPORT);
   orxModule_AddDependency(orxMODULE_ID_RENDER, orxMODULE_ID_DISPLAY);
@@ -90,11 +140,27 @@ orxPLUGIN_END_CORE_FUNCTION_ARRAY(RENDER)
 
 orxSTATUS orxFASTCALL orxRender_Init()
 {
-  return orxPLUGIN_CORE_FUNCTION_POINTER_NAME(orxRender_Init)();
+  orxSTATUS eResult;
+
+  /* Updates result */
+  eResult = orxPLUGIN_CORE_FUNCTION_POINTER_NAME(orxRender_Init)();
+
+  /* Success? */
+  if(eResult != orxSTATUS_FAILURE)
+  {
+    /* Registers all commands */
+    orxRender_RegisterCommands();
+  }
+
+  /* Done! */
+  return eResult;
 }
 
 void orxFASTCALL orxRender_Exit()
 {
+  /* Unregisters commands */
+  orxRender_UnregisterCommands();
+
   orxPLUGIN_CORE_FUNCTION_POINTER_NAME(orxRender_Exit)();
 }
 
