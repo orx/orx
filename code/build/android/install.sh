bfa7cfd3a5f9a772e9f26ef8b77f6817aa95acb3
loki666 <simons.philippe@gmail.com>
2013-04-27 21:38:48 +0200

    android: add support for x86

--- code/build/android/install.sh
+++ code/build/android/install.sh
@@ -2,5 +2,6 @@
 
 cp ./obj/local/armeabi/liborx*.a ../../lib/static/android/armeabi
 cp ./obj/local/armeabi-v7a/liborx*.a ../../lib/static/android/armeabi-v7a
+cp ./obj/local/x86/liborx*.a ../../lib/static/android/x86
 
 exit 0
--- code/demo/android/jni/Application.mk
+++ code/demo/android/jni/Application.mk
@@ -1,3 +1,3 @@
-APP_ABI := armeabi armeabi-v7a
+APP_ABI := armeabi armeabi-v7a x86
 
 APP_MODULES = orxTest
