526c84276a4884d2ac787eb1667e608934b37f6c
iarwain <iarwain@orx-project.org>
2013-10-19 00:47:51 -0400

    - Added orxAnimSet_FindNextAnim()

--- code/include/anim/orxAnimSet.h
+++ code/include/anim/orxAnimSet.h
@@ -209,6 +209,14 @@ extern orxDLLAPI orxU32 orxFASTCALL                 orxAnimSet_GetLinkProperty(c
 */
 extern orxDLLAPI orxU32 orxFASTCALL                 orxAnimSet_ComputeAnim(orxANIMSET *_pstAnimSet, orxU32 _u32SrcAnim, orxU32 _u32DstAnim, orxFLOAT *_pfTime, orxANIMSET_LINK_TABLE *_pstLinkTable, orxBOOL *_pbCut, orxBOOL *_pbClearTarget);
 
+/** Finds next Anim given current and destination Anim IDs
+ * @param[in]   _pstAnimSet                         Concerned AnimSet
+ * @param[in]   _u32SrcAnim                         Source (current) Anim ID
+ * @param[in]   _u32DstAnim                         Destination Anim ID, if none (auto mode) set it to orxU32_UNDEFINED
+ * @return Next Anim ID if found, orxU32_UNDEFINED otherwise
+*/
+extern orxDLLAPI orxU32 orxFASTCALL                 orxAnimSet_FindNextAnim(orxANIMSET *_pstAnimSet, orxU32 _u32SrcAnim, orxU32 _u32DstAnim);
+
 /** AnimSet Anim get accessor
  * @param[in]		_pstAnimSet													Concerned AnimSet
  * @param[in]		_u32AnimID												  Anim ID
