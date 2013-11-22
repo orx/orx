ab2513912c3cc031b16f18586d0f83845ac6a337
loki666 <simons.philippe@gmail.com>
2013-11-22 11:33:47 +0100

    - android: recompile extern libs with gcc 4.8

--- code/build/android/jni/Application.mk
+++ code/build/android/jni/Application.mk
@@ -1,4 +1,3 @@
 APP_ABI := armeabi armeabi-v7a x86
-
 APP_MODULES := orx orxd orxp
-
+NDK_TOOLCHAIN_VERSION := 4.8
--- code/demo/android/jni/Application.mk
+++ code/demo/android/jni/Application.mk
@@ -1,3 +1,3 @@
 APP_ABI := armeabi armeabi-v7a x86
-
 APP_MODULES = orxTest
+NDK_TOOLCHAIN_VERSION := 4.8
