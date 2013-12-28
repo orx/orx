58f663954cfefb1e2b231e0bc903bcbef7db12b8
iarwain <iarwain@orx-project.org>
2013-12-28 16:59:34 -0500

    - Allow profile markers operation only for main thread for now
    --HG--
    branch : thread

--- code/include/debug/orxProfiler.h
+++ code/include/debug/orxProfiler.h
@@ -46,6 +46,8 @@
 
 #include "orxInclude.h"
 
+#include "core/orxThread.h"
+
 
 /* *** Uncomment the line below to enable orx profiling in non debug builds *** */
 //#define __orxPROFILER__
@@ -53,23 +55,36 @@
 
 /** Profiler macros
  */
+//! TODO: Support per-thread profiling info (currently only allowing profiling for main thread)
 #ifdef __orxPROFILER__
 
-  #define orxPROFILER_PUSH_MARKER(NAME)                           \
-  do                                                              \
-  {                                                               \
-    static orxS32 s32ProfilerID = orxPROFILER_KS32_MARKER_ID_NONE;\
-                                                                  \
-    if(orxProfiler_IsMarkerIDValid(s32ProfilerID) == orxFALSE)    \
-    {                                                             \
-      s32ProfilerID = orxProfiler_GetIDFromName(NAME);            \
-    }                                                             \
-                                                                  \
-    orxProfiler_PushMarker(s32ProfilerID);                        \
+  #define orxPROFILER_PUSH_MARKER(NAME)                             \
+  do                                                                \
+  {                                                                 \
+    if((orxModule_IsInitialized(orxMODULE_ID_THREAD) == orxFALSE)   \
+    || (orxThread_GetCurrent() == orxTHREAD_KU32_MAIN_THREAD_ID))   \
+    {                                                               \
+      static orxS32 s32ProfilerID = orxPROFILER_KS32_MARKER_ID_NONE;\
+                                                                    \
+      if(orxProfiler_IsMarkerIDValid(s32ProfilerID) == orxFALSE)    \
+      {                                                             \
+        s32ProfilerID = orxProfiler_GetIDFromName(NAME);            \
+      }                                                             \
+                                                                    \
+      orxProfiler_PushMarker(s32ProfilerID);                        \
+    }                                                               \
   } while(orxFALSE)
 
 
-  #define orxPROFILER_POP_MARKER()                orxProfiler_PopMarker();
+  #define orxPROFILER_POP_MARKER()                                  \
+  do                                                                \
+  {                                                                 \
+    if((orxModule_IsInitialized(orxMODULE_ID_THREAD) == orxFALSE)   \
+    || (orxThread_GetCurrent() == orxTHREAD_KU32_MAIN_THREAD_ID))   \
+    {                                                               \
+      orxProfiler_PopMarker();                                      \
+    }                                                               \
+  } while(orxFALSE)
 
   #define orxPROFILER_KU32_HISTORY_LENGTH         (3 * 60)
 
