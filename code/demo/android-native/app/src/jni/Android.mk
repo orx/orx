7e69499828900ea69e7806507ce6c01666663cf5
loki666 <simons.philippe@gmail.com>
2014-07-03 09:15:08 +0200

    add android-native demo project

---
+++ code/demo/android-native/app/src/jni/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := orxTest
+
+LOCAL_SRC_FILES := orxTest.c
+LOCAL_STATIC_LIBRARIES := orxd
+
+LOCAL_ARM_MODE := arm
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,lib/static/android-native)
+
