e3543e6041bd35eae83a9b19ba5da9a3e1e5445b
sausage <sausage@zeta.org.au>
2014-08-14 13:15:17 +1000

    Windows script version of install.sh for moving compiled android libs.

---
+++ code/build/android/install.bat
@@ -0,0 +1,5 @@
+copy .\obj\local\armeabi\liborx*.a ..\..\lib\static\android\armeabi
+copy .\obj\local\armeabi-v7a\liborx*.a ..\..\lib\static\android\armeabi-v7a
+copy .\obj\local\x86\liborx*.a ..\..\lib\static\android\x86
+
+pause
