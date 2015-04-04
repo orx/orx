219f0dd0998a5276d44057de639cad0876b003e2
orxbot <orxbot@orx-project.org>
2015-04-03 23:26:45 -0400

    - Automatic update of doxygen docs, triggered by buildbot

--- doc/html/group__orx_android.js
+++ doc/html/group__orx_android.js
@@ -4,9 +4,9 @@ var group__orx_android =
       [ "afValues", "structorx_a_n_d_r_o_i_d___a_x_i_s___d_a_t_a.html#abfe8681f6dbfa8ab7410b5e8e48c0755", null ],
       [ "fHAT_X", "structorx_a_n_d_r_o_i_d___a_x_i_s___d_a_t_a.html#a0a4c0e78b576da91191f485bcc501dec", null ],
       [ "fHAT_Y", "structorx_a_n_d_r_o_i_d___a_x_i_s___d_a_t_a.html#adbb1ddb7dadbf506c9f3a1d5d905b71d", null ],
+      [ "fLTRIGGER", "structorx_a_n_d_r_o_i_d___a_x_i_s___d_a_t_a.html#ae33546115c85cdb954dc8ded081f6a40", null ],
+      [ "fRTRIGGER", "structorx_a_n_d_r_o_i_d___a_x_i_s___d_a_t_a.html#a9ac8e98c039f2d2738f9c51438b75f72", null ],
       [ "fRZ", "structorx_a_n_d_r_o_i_d___a_x_i_s___d_a_t_a.html#ae1407f92135645b528b9b0445888842a", null ],
-      [ "fU", "structorx_a_n_d_r_o_i_d___a_x_i_s___d_a_t_a.html#ab18f45e01a1f033eb52da11bac2111c6", null ],
-      [ "fV", "structorx_a_n_d_r_o_i_d___a_x_i_s___d_a_t_a.html#a2bc9e4b1fe71d5561929be03cd861457", null ],
       [ "fX", "structorx_a_n_d_r_o_i_d___a_x_i_s___d_a_t_a.html#a00434c3a488d8efe4635d45568fe7f1f", null ],
       [ "fY", "structorx_a_n_d_r_o_i_d___a_x_i_s___d_a_t_a.html#a34d81313c7181f2c74e5bef41e9b97a5", null ],
       [ "fZ", "structorx_a_n_d_r_o_i_d___a_x_i_s___d_a_t_a.html#a6182fb43603debc63a368f493240466e", null ]
