d7cd6ecf309a5944c7e8c939cc26440c8e3359ed
iarwain <iarwain@orx-project.org>
2015-07-17 00:10:46 -0400

    - Increased the stdio limit to its maximum of 2048 on windows (defaults to 512)

--- code/src/io/orxFile.c
+++ code/src/io/orxFile.c
@@ -249,6 +249,13 @@ orxSTATUS orxFASTCALL orxFile_Init()
     /* Cleans static controller */
     orxMemory_Zero(&sstFile, sizeof(orxFILE_STATIC));
 
+#ifdef __orxWINDOWS__
+
+    /* Increases C runtime stdio limit */
+    _setmaxstdio(2048);
+
+#endif /* __orxWINDOWS__ */
+
     /* Updates status */
     sstFile.u32Flags |= orxFILE_KU32_STATIC_FLAG_READY;
   }
