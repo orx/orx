0e3ce267bedbf07f358249fda22b42b4fbd1e1d0
orxbot <orxbot@orx-project.org>
2016-04-24 01:21:28 -0400

    - Automatic update of doxygen docs, triggered by buildbot

--- doc/html/search/variables_61.js
+++ doc/html/search/variables_61.js
@@ -6,5 +6,6 @@ var searchData=
   ['afvalue',['afValue',['../structorx_i_n_p_u_t___e_v_e_n_t___p_a_y_l_o_a_d.html#a5d66be4dbde3417c65989708e1cf1909',1,'orxINPUT_EVENT_PAYLOAD']]],
   ['afvalues',['afValues',['../structorx_a_n_d_r_o_i_d___a_x_i_s___d_a_t_a.html#abfe8681f6dbfa8ab7410b5e8e48c0755',1,'orxANDROID_AXIS_DATA']]],
   ['as16samplelist',['as16SampleList',['../structorx_s_o_u_n_d___s_t_r_e_a_m___p_a_c_k_e_t.html#a658158c820d64531fda54286dbff2ba6',1,'orxSOUND_STREAM_PACKET']]],
+  ['au8padding',['au8Padding',['../structorx_s_t_r_u_c_t_u_r_e.html#a4e509820748f9c417559fff56f7717d7',1,'orxSTRUCTURE']]],
   ['avvertices',['avVertices',['../structorx_b_o_d_y___p_a_r_t___d_e_f.html#a38748bdfcfda7ada5413939ba16d593b',1,'orxBODY_PART_DEF']]]
 ];
