68199b844a44055c226370bca1ead5130d6079bc
iarwain <iarwain@orx-project.org>
2016-08-26 00:22:20 -0400

    - Added linux 64 version of REBOL 3 binary (from atronix engineering) - Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 OS=`uname -s`
+ARCH=`uname -m`
 
 if [ "${OS}" = "Darwin" ]; then
     REBOL=code/build/rebol/r3-mac
 else
-    REBOL=code/build/rebol/r3-linux
+    if [ "${ARCH}" = "x86_64" ]; then
+        REBOL=code/build/rebol/r3-linux64
+    else
+        REBOL=code/build/rebol/r3-linux
+    fi
 fi
 
 ${REBOL} code/build/rebol/setup.r "$@"
