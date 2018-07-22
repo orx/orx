124fc8a99ef54775c1b5cee7b0c8c78dd174ab0f
iarwain <iarwain@orx-project.org>
2018-07-22 18:04:03 -0400

    - Updated extern (GLFW3/OSX) - Bumped min OSX target: 10.6 -> 10.7 (matching GLFW's) - Added CoreVideo framework to premake scripts
    --HG--
    branch : glfw3

--- code/build/template/build/premake4.lua
+++ code/build/template/build/premake4.lua
@@ -183,13 +183,13 @@ solution "[name]"
     configuration {"macosx"}
         buildoptions
         {
-            "-mmacosx-version-min=10.6",
+            "-mmacosx-version-min=10.7",
             "-gdwarf-2",
             "-Wno-write-strings"
         }
         linkoptions
         {
-            "-mmacosx-version-min=10.6",
+            "-mmacosx-version-min=10.7",
             "-dead_strip"
         }
 
