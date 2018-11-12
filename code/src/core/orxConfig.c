8c6945df2003c0456e25468b89381063887f4dff
iarwain <iarwain@orx-project.org>
2018-11-12 00:33:53 -0500

    - Added command: Config.GetCurrentSection + associated alias: @

--- code/src/core/orxConfig.c
+++ code/src/core/orxConfig.c
@@ -3218,6 +3218,17 @@ void orxFASTCALL orxConfig_CommandClearSection(orxU32 _u32ArgNumber, const orxCO
   return;
 }
 
+/** Command: GetCurrentSection
+ */
+void orxFASTCALL orxConfig_CommandGetCurrentSection(orxU32 _u32ArgNumber, const orxCOMMAND_VAR *_astArgList, orxCOMMAND_VAR *_pstResult)
+{
+  /* Updates result */
+  _pstResult->zValue = orxConfig_GetCurrentSection();
+
+  /* Done! */
+  return;
+}
+
 /** Command: ClearValue
  */
 void orxFASTCALL orxConfig_CommandClearValue(orxU32 _u32ArgNumber, const orxCOMMAND_VAR *_astArgList, orxCOMMAND_VAR *_pstResult)
@@ -3510,6 +3521,8 @@ static orxINLINE void orxConfig_RegisterCommands()
   orxCOMMAND_REGISTER_CORE_COMMAND(Config, HasSection, "Section?", orxCOMMAND_VAR_TYPE_BOOL, 1, 0, {"Section", orxCOMMAND_VAR_TYPE_STRING});
   /* Command: ClearSection */
   orxCOMMAND_REGISTER_CORE_COMMAND(Config, ClearSection, "Section", orxCOMMAND_VAR_TYPE_STRING, 1, 0, {"Section", orxCOMMAND_VAR_TYPE_STRING});
+  /* Command: GetCurrentSection */
+  orxCOMMAND_REGISTER_CORE_COMMAND(Config, GetCurrentSection, "Section", orxCOMMAND_VAR_TYPE_STRING, 0, 0);
 
   /* Command: HasValue */
   orxCOMMAND_REGISTER_CORE_COMMAND(Config, HasValue, "Value?", orxCOMMAND_VAR_TYPE_BOOL, 2, 0, {"Section", orxCOMMAND_VAR_TYPE_STRING}, {"Key", orxCOMMAND_VAR_TYPE_STRING});
@@ -3539,6 +3552,9 @@ static orxINLINE void orxConfig_RegisterCommands()
   orxCommand_AddAlias("Get", "Config.GetValue", orxNULL);
   /* Alias: GetRaw */
   orxCommand_AddAlias("GetRaw", "Config.GetRawValue", orxNULL);
+
+  /* Alias: @ */
+  orxCommand_AddAlias("@", "Config.GetCurrentSection", orxNULL);
 }
 
 /** Unregisters all the config commands
@@ -3559,6 +3575,9 @@ static orxINLINE void orxConfig_UnregisterCommands()
   /* Alias: GetRaw */
   orxCommand_RemoveAlias("GetRaw");
 
+  /* Alias: @ */
+  orxCommand_RemoveAlias("@");
+
   /* Command: Load */
   orxCOMMAND_UNREGISTER_CORE_COMMAND(Config, Load);
   /* Command: Save */
@@ -3578,6 +3597,8 @@ static orxINLINE void orxConfig_UnregisterCommands()
   orxCOMMAND_UNREGISTER_CORE_COMMAND(Config, HasSection);
   /* Command: ClearSection */
   orxCOMMAND_UNREGISTER_CORE_COMMAND(Config, ClearSection);
+  /* Command: GetCurrentSection */
+  orxCOMMAND_UNREGISTER_CORE_COMMAND(Config, GetCurrentSection);
 
   /* Command: HasValue */
   orxCOMMAND_UNREGISTER_CORE_COMMAND(Config, HasValue);
