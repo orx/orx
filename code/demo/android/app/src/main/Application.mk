4773e70f3850a80a0fad6aae058596a721f98ba5
Andrew <andrew@danvd.me>
2018-11-18 22:59:35 +0400

    Update android demo to gradle 4.6, ndk r18b, gradle plugin 3.2.1, android support 26.1.0, minimum API 19.

---
+++ code/demo/android/app/src/main/Application.mk
@@ -0,0 +1,5 @@
+APP_ABI := armeabi-v7a x86 arm64-v8a
+APP_MODULES := orxTest
+NDK_TOOLCHAIN_VERSION := clang
+APP_PLATFORM := android-19
+APP_STL := c++_shared
--- code/demo/android/app/src/main/jni/Android.mk
+++ code/demo/android/app/src/main/jni/Android.mk
@@ -4,6 +4,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := orxTest
 
+$(call import-add-path,$(LOCAL_PATH)/../../../../../../)
+
 LOCAL_SRC_FILES := orxTest.c
 LOCAL_STATIC_LIBRARIES := orxd
 
