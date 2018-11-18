fa492d597992cb2ab7e3e9cc17bae6e97abcb820
Andrew <andrew@danvd.me>
2018-11-18 23:12:34 +0400

    Update android native demo to gradle 4.6, ndk r18b, gradle plugin 3.2.1, android support 26.1.0, minimum API 19.

--- code/demo/android-native/app/src/main/jni/Android.mk
+++ code/demo/android-native/app/src/main/jni/Android.mk
@@ -3,6 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := orxTest
+$(call import-add-path,$(LOCAL_PATH)/../../../../../../)
 
 LOCAL_SRC_FILES := orxTest.c
 LOCAL_STATIC_LIBRARIES := orxd
