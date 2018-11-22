43882d9efeb6636394525dfbce0798fd08ed6d54
Andrew <andrew@danvd.me>
2018-11-22 23:21:29 +0400

    Adding x86_64 android target for orx

--- code/build/android/install.sh
+++ code/build/android/install.sh
@@ -3,5 +3,6 @@
 cp ./obj/local/armeabi-v7a/liborx*.a ../../lib/static/android/armeabi-v7a
 cp ./obj/local/arm64-v8a/liborx*.a ../../lib/static/android/arm64-v8a
 cp ./obj/local/x86/liborx*.a ../../lib/static/android/x86
+cp ./obj/local/x86_64/liborx*.a ../../lib/static/android/x86_64
 
 exit 0
