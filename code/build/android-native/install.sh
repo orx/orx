353a149e08d8ec9d3bb12bbab3d8c6d8b7ec3f7a
Andrew <andrew@danvd.me>
2018-11-22 23:18:56 +0400

    Adding x86_64 android-native target for orx

--- code/build/android-native/install.sh
+++ code/build/android-native/install.sh
@@ -3,5 +3,6 @@
 cp ./obj/local/armeabi-v7a/liborx*.a ../../lib/static/android-native/armeabi-v7a
 cp ./obj/local/arm64-v8a/liborx*.a ../../lib/static/android-native/arm64-v8a
 cp ./obj/local/x86/liborx*.a ../../lib/static/android-native/x86
+cp ./obj/local/x86_64/liborx*.a ../../lib/static/android-native/x86_64
 
 exit 0
