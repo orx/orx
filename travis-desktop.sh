da372c680a7881e64655f440792a435d68324ea2
loki666 <simons.philippe@gmail.com>
2019-10-14 11:36:31 +0200

    use

--- travis-desktop.sh
+++ travis-desktop.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 set -ev
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    cd ~/build/orx/orx/code/build/mac/gmake ;
+    cd $TRAVIS_BUILD_DIR/code/build/mac/gmake ;
 else
-  cd ~/build/orx/orx/code/build/linux/gmake ;
+  cd $TRAVIS_BUILD_DIR/code/build/linux/gmake ;
 fi
 make config=debug64
 make config=profile64
 make config=release64
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  cd ~/build/orx/orx/tutorial/build/mac/gmake ;
+  cd $TRAVIS_BUILD_DIR/tutorial/build/mac/gmake ;
 else
-  cd ~/build/orx/orx/tutorial/build/linux/gmake ;
+  cd $TRAVIS_BUILD_DIR/tutorial/build/linux/gmake ;
 fi
 make config=release64
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  cd ~/build/orx/orx/code/build/mac/gmake ;
+  cd $TRAVIS_BUILD_DIR/code/build/mac/gmake ;
 else
-  cd ~/build/orx/orx/code/build/linux/gmake ;
+  cd $TRAVIS_BUILD_DIR/code/build/linux/gmake ;
 fi
 make config=core_release64
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  cd ~/build/orx/orx/tools/orxCrypt/build/mac/gmake ;
+  cd $TRAVIS_BUILD_DIR/tools/orxCrypt/build/mac/gmake ;
 else
-  cd ~/build/orx/orx/tools/orxCrypt/build/linux/gmake ;
+  cd $TRAVIS_BUILD_DIR/tools/orxCrypt/build/linux/gmake ;
 fi
 make config=release64
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  cd ~/build/orx/orx/tools/orxFontGen/build/mac/gmake ;
+  cd $TRAVIS_BUILD_DIR/tools/orxFontGen/build/mac/gmake ;
 else
-  cd ~/build/orx/orx/tools/orxFontGen/build/linux/gmake ;
+  cd $TRAVIS_BUILD_DIR/tools/orxFontGen/build/linux/gmake ;
 fi
 make config=release64
-cd ~/build/orx/orx/code/build/python
+cd $TRAVIS_BUILD_DIR/code/build/python
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   python package.py -p mac &&
   python package.py -p tutomac;
