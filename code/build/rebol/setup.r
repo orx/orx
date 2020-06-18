ea7e1cb75d914874cc0cc038762949e827bd7aca
iarwain <iarwain@orx-project.org>
2020-06-18 00:26:43 -0400

    - Added support for zsh to setup script

--- code/build/rebol/setup.r
+++ code/build/rebol/setup.r
@@ -140,6 +140,7 @@ either platform = "windows" [
   for-each [env-file env-prefix mandatory] reduce [
     env-home/.bashrc                      rejoin ["export " env-variable "="]   true
     env-home/.profile                     rejoin ["export " env-variable "="]   true
+    env-home/.zshrc                       rejoin ["export " env-variable "="]   false
     env-home/.config/fish/fish_variables  rejoin ["SETUVAR " env-variable ":"]  false
   ] [
     if any [mandatory exists? env-file] [
