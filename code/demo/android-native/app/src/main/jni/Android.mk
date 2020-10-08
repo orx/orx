c5831776e081f92135c3f051df22879c4e38047a
l33t <kristoffer>
2020-10-08 21:56:42 +0200

    Improved Android Native build scripts

--- code/demo/android-native/app/src/main/jni/Android.mk
+++ code/demo/android-native/app/src/main/jni/Android.mk
@@ -5,9 +5,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := orxTest
 
 LOCAL_SRC_FILES := orxTest.cpp
-LOCAL_STATIC_LIBRARIES := orx
+LOCAL_STATIC_LIBRARIES := orx_native
 LOCAL_ARM_MODE := arm
 
 include $(BUILD_SHARED_LIBRARY)
-$(call import-add-path,$(LOCAL_PATH)/../../../../../../)
+$(call import-add-path,$(ORX))
 $(call import-module,lib/static/android-native)
