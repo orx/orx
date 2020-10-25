80c4af462253c2dc03b292ba0ff8bf0df2e7c65c
l33t <kristoffer>
2020-10-25 22:51:21 +0100

    Non-native Android demo app now automatically builds non-native orx (both C lib and Java orx-lib).

--- code/demo/android/app/src/main/jni/Android.mk
+++ code/demo/android/app/src/main/jni/Android.mk
@@ -1,16 +1,17 @@
 LOCAL_PATH := $(call my-dir)
 
+ORX := $(or $(ORX),$(LOCAL_PATH)/../../../../../..)
+
+SRC_PATH := $(LOCAL_PATH)
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := orxTest
 
-$(call import-add-path,$(LOCAL_PATH)/../../../../../../)
-
-LOCAL_SRC_FILES := orxTest.cpp
-LOCAL_STATIC_LIBRARIES := orx
-
+LOCAL_SRC_FILES := $(SRC_PATH)/orxTest.cpp
+LOCAL_STATIC_LIBRARIES := android_orx
 LOCAL_ARM_MODE := arm
 
 include $(BUILD_SHARED_LIBRARY)
-
+$(call import-add-path,$(ORX))
 $(call import-module,lib/static/android)
--- code/demo/android/app/src/main/jni/Application.mk
+++ code/demo/android/app/src/main/jni/Application.mk
@@ -1,5 +1,6 @@
 APP_ABI := armeabi-v7a x86 arm64-v8a x86_64
-APP_MODULES := orxTest
-NDK_TOOLCHAIN_VERSION := clang
-APP_PLATFORM := android-19
-APP_STL:=c++_shared
+APP_MODULES = orxTest
+APP_PLATFORM = android-17
+
+# For single-lib apps, linking STL statically reduces the APK size significantly.
+APP_STL:=c++_static
