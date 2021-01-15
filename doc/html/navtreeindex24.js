772b9f4dea53fe0981bbc5e3953c0c078aca654e
orxbot <orxbot@orx-project.org>
2021-01-15 10:38:29 -0500

    - Automatic update of doxygen docs, triggered by buildbot

--- doc/html/navtreeindex24.js
+++ doc/html/navtreeindex24.js
@@ -1,5 +1,9 @@
 var NAVTREEINDEX24 =
 {
+"orx_plugin_8h.html":[3,0,0,11,1],
+"orx_plugin_8h_source.html":[3,0,0,11,1],
+"orx_plugin___core_define_8h.html":[3,0,0,11,0,0],
+"orx_plugin___core_define_8h_source.html":[3,0,0,11,0,0],
 "orx_plugin___core_i_d_8h.html":[3,0,0,11,0,1],
 "orx_plugin___core_i_d_8h_source.html":[3,0,0,11,0,1],
 "orx_plugin___display_8h.html":[3,0,0,11,0,2],
@@ -245,9 +249,5 @@ var NAVTREEINDEX24 =
 "structorx_d_i_s_p_l_a_y___m_e_s_h.html#a8544c4ecc68c9159022c28be377b16e7":[1,15,4,2],
 "structorx_d_i_s_p_l_a_y___m_e_s_h.html#a9311c22fe1a3e613eac78b54e4c977f2":[1,15,4,1],
 "structorx_d_i_s_p_l_a_y___m_e_s_h.html#acdf7b863cc8cc6f7c6086f53926a7327":[1,15,4,4],
-"structorx_d_i_s_p_l_a_y___t_r_a_n_s_f_o_r_m.html":[1,15,5],
-"structorx_d_i_s_p_l_a_y___t_r_a_n_s_f_o_r_m.html#a144f988cdf6844f63cbc219180af1c84":[1,15,5,3],
-"structorx_d_i_s_p_l_a_y___t_r_a_n_s_f_o_r_m.html#a3ac91be7c665e8b71b6fc28074bfd89b":[1,15,5,4],
-"structorx_d_i_s_p_l_a_y___t_r_a_n_s_f_o_r_m.html#a3c0a7cf0d8e8730e8f7f57a6cba4430b":[1,15,5,1],
-"structorx_d_i_s_p_l_a_y___t_r_a_n_s_f_o_r_m.html#a43768f29fd84462567cb4f82840a7338":[1,15,5,5]
+"structorx_d_i_s_p_l_a_y___t_r_a_n_s_f_o_r_m.html":[1,15,5]
 };
