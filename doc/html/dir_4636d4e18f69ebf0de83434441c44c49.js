84e7228343672e8721b5efdeaa5301bd95d0b6dd
orxbot <orxbot@orx-project.org>
2021-02-24 23:58:42 -0500

    - Automatic update of doxygen docs, triggered by buildbot

--- doc/html/dir_4636d4e18f69ebf0de83434441c44c49.js
+++ doc/html/dir_4636d4e18f69ebf0de83434441c44c49.js
@@ -1,5 +1,6 @@
 var dir_4636d4e18f69ebf0de83434441c44c49 =
 [
+    [ "orxColorList.inc", "orx_color_list_8inc.html", null ],
     [ "orxDisplay.h", "orx_display_8h.html", "orx_display_8h" ],
     [ "orxFont.h", "orx_font_8h.html", "orx_font_8h" ],
     [ "orxGraphic.h", "orx_graphic_8h.html", "orx_graphic_8h" ],
--- doc/html/navtreeindex25.js
+++ doc/html/navtreeindex25.js
@@ -1,15 +1,5 @@
 var NAVTREEINDEX25 =
 {
-"structorx_d_i_s_p_l_a_y___t_r_a_n_s_f_o_r_m.html#a144f988cdf6844f63cbc219180af1c84":[1,15,5,3],
-"structorx_d_i_s_p_l_a_y___t_r_a_n_s_f_o_r_m.html#a3ac91be7c665e8b71b6fc28074bfd89b":[1,15,5,4],
-"structorx_d_i_s_p_l_a_y___t_r_a_n_s_f_o_r_m.html#a3c0a7cf0d8e8730e8f7f57a6cba4430b":[1,15,5,1],
-"structorx_d_i_s_p_l_a_y___t_r_a_n_s_f_o_r_m.html#a43768f29fd84462567cb4f82840a7338":[1,15,5,5],
-"structorx_d_i_s_p_l_a_y___t_r_a_n_s_f_o_r_m.html#a4ba0f2b96592334b5021588e4f7d0a72":[1,15,5,2],
-"structorx_d_i_s_p_l_a_y___t_r_a_n_s_f_o_r_m.html#a54b8a1dee3c659bf3abdfff81b78bb75":[1,15,5,0],
-"structorx_d_i_s_p_l_a_y___t_r_a_n_s_f_o_r_m.html#a7f926a090e7f4930b448a83b77e2ec8c":[1,15,5,6],
-"structorx_d_i_s_p_l_a_y___t_r_a_n_s_f_o_r_m.html#aabce29f786fcf92b79507c596990c886":[1,15,5,8],
-"structorx_d_i_s_p_l_a_y___t_r_a_n_s_f_o_r_m.html#ae53a9736f1e76f092d0dec336672c0a4":[1,15,5,7],
-"structorx_d_i_s_p_l_a_y___v_e_r_t_e_x.html":[1,15,6],
 "structorx_d_i_s_p_l_a_y___v_e_r_t_e_x.html#a3993f6f12422b28e5252bcaac7759fee":[1,15,6,0],
 "structorx_d_i_s_p_l_a_y___v_e_r_t_e_x.html#a45e72efa75f7322e82e283e59197aaf3":[1,15,6,1],
 "structorx_d_i_s_p_l_a_y___v_e_r_t_e_x.html#a532187272201cfb0496289d5ec06a030":[1,15,6,4],
