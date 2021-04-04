0579781f9a3ac4769218f373b8eec61b98978e50
iarwain <iarwain@orx-project.org>
2021-04-04 14:01:19 -0400

    - Added init-ed project to Travis-CI script
    --HG--
    branch : object_update_optimization

--- code/build/travis/desktop.sh
+++ code/build/travis/desktop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ev
+# Library/embedded
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   cd $TRAVIS_BUILD_DIR/code/build/mac/gmake ;
 else
@@ -8,18 +9,21 @@ fi
 make config=debug64
 make config=profile64
 make config=release64
+# Tutorials
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   cd $TRAVIS_BUILD_DIR/tutorial/build/mac/gmake ;
 else
   cd $TRAVIS_BUILD_DIR/tutorial/build/linux/gmake ;
 fi
 make config=release64
+# Library/core
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   cd $TRAVIS_BUILD_DIR/code/build/mac/gmake ;
 else
   cd $TRAVIS_BUILD_DIR/code/build/linux/gmake ;
 fi
 make config=core_release64
+# Tools
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   cd $TRAVIS_BUILD_DIR/tools/orxCrypt/build/mac/gmake ;
 else
@@ -32,6 +36,7 @@ else
   cd $TRAVIS_BUILD_DIR/tools/orxFontGen/build/linux/gmake ;
 fi
 make config=release64
+# Package
 cd $TRAVIS_BUILD_DIR/code/build/python
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   python package.py -p mac &&
@@ -40,3 +45,15 @@ else
   python package.py -p linux64 &&
   python package.py -p tutolinux64;
 fi
+# Init
+cd $TRAVIS_BUILD_DIR
+export ORX=$TRAVIS_BUILD_DIR/code
+./init.sh test +scroll +imgui
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  cd $TRAVIS_BUILD_DIR/test/build/mac/gmake ;
+else
+  cd $TRAVIS_BUILD_DIR/test/build/linux/gmake ;
+fi
+make config=debug64
+make config=profile64
+make config=release64
