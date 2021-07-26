a6bdc9ede3225899915b9eec0857b9c8e371862f
iarwain <iarwain@orx-project.org>
2021-07-25 23:17:16 -0400

    - Updated movie init extension to be compatible with Scroll
    --HG--
    branch : movie

--- code/build/rebol/init.r
+++ code/build/rebol/init.r
@@ -11,7 +11,7 @@ params: [
   archive     {orxArchive support (resources can be stored inside ZIP files)}         -       []
   c++         {Create a C++ project instead of C}                                     +       []
   imgui       {Dear ImGui support (https://github.com/ocornut/imgui)}                 -       [+c++]
-  movie       {Movie (MPEG-1) decoding support}                                       -       [-scroll]
+  movie       {Movie (MPEG-1) decoding support}                                       -       []
   nuklear     {Nuklear support (https://github.com/immediate-mode-ui/nuklear)}        -       []
   scroll      {C++ convenience layer with config-object binding}                      -       [+c++]
 ]
