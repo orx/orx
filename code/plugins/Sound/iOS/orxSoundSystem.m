4940780579755922c96111ba027968f313e6d5fd
iarwain <iarwain@orx-project.org>
2021-11-10 04:25:55 -0500

    - Silenced compilation warnings on iOS with MiniAudio soundsystem plugin
    --HG--
    branch : miniaudio

--- code/plugins/Sound/iOS/orxSoundSystem.m
+++ code/plugins/Sound/iOS/orxSoundSystem.m
@@ -31,5 +31,13 @@
  *
  */
 
+#ifdef __orxLLVM__
+  #pragma clang diagnostic push
+  #pragma clang diagnostic ignored "-Wformat"
+#endif /* __orxLLVM__ */
 
 #include "../plugins/Sound/MiniAudio/orxSoundSystem.c"
+
+#ifdef __orxLLVM__
+  #pragma clang diagnostic pop
+#endif /* __orxLLVM__ */
