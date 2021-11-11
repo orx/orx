c33e09dff74554776c372f9c1781532c7aafe38a
iarwain <iarwain@orx-project.org>
2021-11-11 01:12:44 -0500

    - Removed obsolete code to silence warnings with MiniAudio soundsystem
    --HG--
    branch : miniaudio

--- code/plugins/Sound/iOS/orxSoundSystem.m
+++ code/plugins/Sound/iOS/orxSoundSystem.m
@@ -31,13 +31,4 @@
  *
  */
 
-#ifdef __orxLLVM__
-  #pragma clang diagnostic push
-  #pragma clang diagnostic ignored "-Wformat"
-#endif /* __orxLLVM__ */
-
 #include "../plugins/Sound/MiniAudio/orxSoundSystem.c"
-
-#ifdef __orxLLVM__
-  #pragma clang diagnostic pop
-#endif /* __orxLLVM__ */
