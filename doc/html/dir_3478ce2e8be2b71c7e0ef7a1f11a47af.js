7fa5e1d5958753b5feb809729da6534ab3420b85
orxbot <orxbot@orx-project.org>
2023-02-02 23:24:28 -0500

    - Automatic update of doxygen docs, triggered by buildbot

---
+++ doc/html/dir_3478ce2e8be2b71c7e0ef7a1f11a47af.js
@@ -0,0 +1,5 @@
+var dir_3478ce2e8be2b71c7e0ef7a1f11a47af =
+[
+    [ "orxAndroid.h", "orx_android_8h.html", "orx_android_8h" ],
+    [ "orxAndroidActivity.h", "orx_android_activity_8h.html", "orx_android_activity_8h" ]
+];
---
+++ doc/html/orx_android_activity_8h.js
@@ -0,0 +1,5 @@
+var orx_android_activity_8h =
+[
+    [ "orxANDROID_KU32_MAX_JOYSTICK_NUMBER", "group__orx_android.html#ga6b733850fcd358e5fc35c4aca02abc69", null ],
+    [ "orxAndroid_GetGameActivity", "group__orx_android.html#ga09ba161b9701411f9c63e2749a2e286e", null ]
+];
