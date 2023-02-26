4059f439cb8a97b46f8fdc3871fe125c2f7d7cad
znakeeye <hacker.danielsson@gmail.com>
2023-02-26 21:17:24 +0100

    Improved build script. Now publishing AAR directly into orx specific folder.

--- code/build/android/build.bat
+++ code/build/android/build.bat
@@ -1,5 +1,1 @@
-gradlew build publishToMavenLocal
-
-xcopy /S /I %USERPROFILE%\.m2\repository\org\orx-project ..\..\lib\static\android\repository\org\orx-project
-
-pause
+gradlew build publishAllPublicationsToOrxRepository
--- code/build/android/build.sh
+++ code/build/android/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-./gradlew build publishToMavenLocal
-
-mkdir -p ../../lib/static/android/repository/org/orx-project
-cp -rf ~/.m2/repository/org/orx-project ../../lib/static/android/repository/org
+./gradlew build publishAllPublicationsToOrxRepository
 
 exit 0
