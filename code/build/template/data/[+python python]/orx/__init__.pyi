bf3d7c36d6099567fa3daf9796fde59e2c40273a
Hezekiah M. Carty <hez@0ok.org>
2024-10-26 14:57:49 -0600

    Updated Python extension:
    - Update __init__.py to automatically expose orx.* modules. The code works without this, but type checkers like pyright function better with it.

--- code/build/template/data/[+python python]/orx/__init__.pyi
+++ code/build/template/data/[+python python]/orx/__init__.pyi
@@ -1,1 +1,5 @@
+# Expose each submodule as `orx.<module_name>`
+from . import command, config, input, object, vector
+
+# Log a message to the terminal and console using orx's logging
 def log(message: str) -> None: ...
