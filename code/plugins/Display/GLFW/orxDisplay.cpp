3f7686d836f01e1a9063ccee18bdcf4a68cdccde
iarwain <iarwain@orx-project.org>
2025-04-13 00:13:34 -0400

    - Updated all display plugins:   . Font creation will now honor Display.MaxTextureSize if available (used as maximum font texture width)   -> This permits temporary overrides for specific use cases

--- code/plugins/Display/GLFW/orxDisplay.cpp
+++ code/plugins/Display/GLFW/orxDisplay.cpp
@@ -5482,7 +5482,12 @@ orxBITMAP *orxFASTCALL orxDisplay_GLFW_LoadFont(const orxSTRING _zFileName, cons
                       const orxSTRING zCharacterList;
                       orxU32          u32CharacterCodePoint, i;
                       int             iX0, iX1, iY0, iY1;
-                      orxFLOAT        fCurrentWidth, fWidth, fHeight, fBaseLine, fXPadding, fYPadding;
+                      orxFLOAT        fCurrentWidth, fWidth, fHeight, fBaseLine, fXPadding, fYPadding, fMaxTextureWidth;
+
+                      /* Gets max texture width */
+                      orxConfig_PushSection(orxDISPLAY_KZ_CONFIG_SECTION);
+                      fMaxTextureWidth = (orxConfig_HasValue(orxDISPLAY_KZ_CONFIG_MAX_TEXTURE_SIZE) != orxFALSE) ? orxConfig_GetFloat(orxDISPLAY_KZ_CONFIG_MAX_TEXTURE_SIZE) : orxS2F(sstDisplay.iMaxTextureSize);
+                      orxConfig_PopSection();
 
                       /* Stores source buffer */
                       pstLoadInfo->pu8Buffer = pu8Buffer;
@@ -5562,7 +5567,7 @@ orxBITMAP *orxFASTCALL orxDisplay_GLFW_LoadFont(const orxSTRING _zFileName, cons
                         fAdvance = pstLoadInfo->astGlyphList[i].stGlyph.fWidth + _pvCharacterSpacing->fX;
 
                         /* Updates dimensions */
-                        if(fCurrentWidth + fAdvance <= orxS2F(sstDisplay.iMaxTextureSize))
+                        if(fCurrentWidth + fAdvance <= fMaxTextureWidth)
                         {
                           fCurrentWidth += fAdvance;
                         }
