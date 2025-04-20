65cf82898758ab7fd6c38b925fac1442ac8786be
Hezekiah M. Carty <hez@0ok.org>
2025-04-19 22:29:24 -0600

    Updated Python extension:
    - Add on_camera_update callback in Python to match CameraUpdate in C and C++ projects
    - Update init project to match upstream C and C++ changes
    - Add orx.vector.round and orx.vector.Vector.round

--- code/build/template/data/[+python python]/[=name].py
+++ code/build/template/data/[+python python]/[=name].py
@@ -3,7 +3,16 @@ import miniscroll, orx
 class Logo(miniscroll.Mini):
   def on_update(self, dt: float):
     if orx.input.has_been_activated("Reverse"):
+      # Reverse logo's rotation
       self.o.set_angular_velocity(self.o.get_angular_velocity() * -1)
+
+      # Get camera proxy control object
+      with orx.config.Section("MainCamera"):
+        id = orx.config.get_guid("ID")
+      camera = orx.object.from_guid(id)
+      if camera is not None:
+        # Add shake effect to the camera
+        camera.fire_trigger("MiniShake")
 
 def init():
   miniscroll.classes = {
@@ -17,6 +26,20 @@ def update(dt: float):
   # Check to see if we should quit the game
   if orx.input.has_been_activated("Quit"):
     orx.close()
+
+def camera_update(dt: float):
+  # Get camera proxy control object
+  with orx.config.Section("MainCamera"):
+    id = orx.config.get_guid("ID")
+  camera = orx.object.from_guid(id)
+  if camera is not None:
+    # Round camera position
+    position = camera.get_position()
+    position.round()
+    camera.set_position(position)
+
+# Register camera update callback
+orx.on_camera_update = camera_update
 
 # Setup engine callbacks for miniscroll
 miniscroll.setup(init, update)
